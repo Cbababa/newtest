feff5dec162be0196d896e851cf6440c556ff09b
Cbababa <100921907+Cbababa@users.noreply.github.com>
2023-10-25 17:41:53 +0800

    upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-design-01-19102022-draft02c",
+      "name": "Design 01 19102022 draft02c",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -3.108227966122529,
+        "pitch": -0.035068254215630645,
+        "fov": 1.569304376149666
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.8710026700078544,
+          "pitch": 0.3811427530904794,
+          "title": "Ask",
+          "text": "Ask me anything"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
